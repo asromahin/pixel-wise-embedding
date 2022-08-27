d4be802dae8e34780733e41aefeadfb5d2ef1143
asromahin <asromahin@yandex.ru>
2022-08-27 16:44:16 +0700

    skip zero

--- datasets/ade20k.py
+++ datasets/ade20k.py
@@ -10,33 +10,6 @@ from datasets.ade20k_utils import loadAde20K
 from datasets.ade20k_classes import ade20k_class_to_code
 
 
-class ADE20KDataset:
-    def __init__(self, images, transforms, mask_key='class_mask'):
-        self.images = images
-        self.transforms = transforms
-        self.mask_key =  mask_key
-
-    def __len__(self):
-        return len(self.images)
-
-    def __getitem__(self, idx):
-        image_name = self.images[idx]
-        image = cv2.imread(image_name)
-        try:
-            res = loadAde20K(image_name)
-        except:
-            return self[idx + 1]
-        mask = res[self.mask_key]
-
-        a = self.transforms(image=image, mask=mask)
-        image = a['image']
-        mask = a['mask']
-
-        image = ToTensor()(image)
-        mask = torch.tensor(mask, dtype=torch.long)
-        return image, mask
-
-
 class ADE20KObjectsDataset:
     def __init__(self, images, transforms):
         self.images = images
@@ -58,7 +31,7 @@ class ADE20KObjectsDataset:
 
         for i in range(len(data['class'])):
             cur_class = data['class'][i]
-            cur_class_idx = ade20k_class_to_code[cur_class]
+            cur_class_idx = ade20k_class_to_code[cur_class] + 1
             cur_poly = data['polygon'][i]
             x = cur_poly['x']
             y = cur_poly['y']
